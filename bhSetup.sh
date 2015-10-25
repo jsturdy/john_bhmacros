6f51bb56633b84cdbf4c0708ffde57884b72174b
John Hakala <john_hakala@brown.edu>
2015-10-25 12:39:29 +0100

    add functionality for splitting jobs up by file,make environment setup easy

--- bhSetup.sh
+++ bhSetup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# This setup script takes one argument -- the path to your CMSSW's src directory. This is needed to set up a current version of ROOT on lxplus since it has an outdated version by default. 
+# The other thing this script does is set some custom python tools up.
+# Example: source bhSetup.sh /afs/cern.ch/user/j/johakala/work/public/CMSSW_7_4_14/src
+if [ "$#" -ne 1 ]; then
+    echo "Please supply the path to your CMSSW area's 'src' directory"
+    echo "Usage Example:"
+    echo "source bhSetup.sh /afs/cern.ch/user/j/johakala/work/public/CMSSW_7_4_14/src"
+fi
+MYDIR=$PWD
+cd $1
+cmsenv
+cd $MYDIR
 export PYTHONPATH=$PYTHONPATH:$PWD/mctools:$PWD/macrotools
+
--- macrotools/macrotools.py
+++ macrotools/macrotools.py
@@ -1,5 +1,41 @@
 import subprocess
+import math
 
 def callMacro(macroName, inputFile):
 	macroCommand = "%s(\"%s\")" % (macroName, inputFile)
 	subprocess.call(["root", "-l", "-q", macroCommand])
+
+def splitJobsForBsub(dataOrMC, inputFile, numberOfJobs):
+	if dataOrMC == "data":
+		samplesListsDir="samplesLists_data"
+	if dataOrMC == "mc":
+		samplesListsDir="samplesLists_mc"
+	else:
+		print "Please choose either 'data' or 'mc'." 
+		exit(1)
+	inputFilePath = samplesListsDir+"/"+inputFile
+	num_input_samples = sum(1 for line in open(inputFilePath))
+	with open(inputFilePath) as inputSamplesList:
+		print "number of input samples is %i" % num_input_samples
+		chunksize = math.ceil(num_input_samples / numberOfJobs)
+		print "chunksize is: %i" % chunksize
+		#chunksize = 2
+		fid = 1
+		lineswritten = 0
+		f = open(samplesListsDir+"/splitLists/"+'%s_split_%i.tx'%(inputFile, fid), 'w')
+		for i,line in enumerate(inputSamplesList):
+			print "%i : \n      %s" % (i, line)
+			f.write(line)
+			lineswritten+=1
+			if lineswritten == chunksize and fid<=numberOfJobs:
+				f.close()
+				fid += 1
+				lineswritten = 0
+				f = open(samplesListsDir+"/splitLists/"+'%s_split_%i.tx'%(inputFile, fid), 'w')
+		f.close()
+		return fid
+
+def bSubSplitJobs(macroname, dataOrMC, inputFile, numberOfJobs):
+	#splitJobsForBsub(dataOrMC, inputFile, numberOfJobs)
+	nJobs = splitJobsForBsub(dataOrMC, inputFile, numberOfJobs)
+	#print "Going to submit %i jobs on bsub." % nJobs
